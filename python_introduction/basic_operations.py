02f46396869192c747a376d2104b83bc4079e7e2
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-10 13:48:08 +0300

    Added Arithmetic symbols

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,9 @@
 number1 = 10
 number2 = 5
+10 + 5 = 15
+10 - 5 = 5
+10 * 5 = 50
 print("Addition of 10 and 5 is 15")
 print("Subtraction of 10 and 5 is 5")
 print("Multiplication of 10 and 5 is 50")
+
