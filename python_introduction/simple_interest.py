a38366fca8370c3321de6df237ae9d868423fc1b
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-10 22:03:10 +0300

    Changed file 2

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,6 @@
-I = P *R * T
-P = $1000
-rate\s*=\s*0\.05 
-principal\s*=\s*1000
-time\s*=\s*3
-R = 5%
-T = 3years
-I = 1000 * 0.05 * 3 = 150.0
-print("The simple interest is: 150.0:")
-
+principal = 1000
+rate = 0.05
+time = 3
+I = P * R * T
+interest = 150.0
+print("The simple interest is: {interest}")
