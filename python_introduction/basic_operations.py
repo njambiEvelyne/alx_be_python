1cf218516ddd57461e1da544cd791c4027c38684
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-10 13:57:12 +0300

    Done correction on the execution Statements

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
 number1 = 10
 number2 = 5
-10 + 5 = 15
-10 - 5 = 5
-10 * 5 = 50
+result = number1 + number2 
+difference = number1 - number2
+product = number1 * number2
 print("Addition of 10 and 5 is 15")
 print("Subtraction of 10 and 5 is 5")
 print("Multiplication of 10 and 5 is 50")
