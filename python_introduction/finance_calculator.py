906b548b6a3a4028d17596da58b88475d985f87e
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-10 22:51:35 +0300

    Correction

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,3 +1,4 @@
 print(input("Enter your monthly income:"))
 print(input("Enter your monthly expenses:"))
 Monthly Savings = monthly income - monthly expenses
+monthly_savings\s*=\s*(monthly_income\s*-\s*monthly_expenses|float\s*\(\s*monthly_income\s*\)\s*-\s*float\s*\(\s*monthly_expenses\s*\))
