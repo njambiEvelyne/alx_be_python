8a34c64acc9949ec676a462d3c7212abffd7d1ff
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-12 21:11:21 +0300

    Execution correction

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,7 @@
 principal = 1000
 rate = 0.05
 time = 3
-I = P * R * T
 interest = principal * rate * time
-1000 * 0.05 * 3 = 150.0
-interest = 150.0
-interest =(1000*0.05*3 =150.0)
-print("The simple interest is: 150.0")
+print(f"The simple interest is: {interest}")
+
 
