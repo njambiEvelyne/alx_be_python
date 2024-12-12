b1d7ee70132f9160df7c3f2ee6aa67fa26dc8be9
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-12 07:30:22 +0300

    Added formula

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -8,6 +8,7 @@ interest\s*=\s*principal\s*\*\s*time\s*\*\s*rate
 interest\s*=\s*time\s*\*\s*principal\s*\*\s*rate
 interest\s*=\s*rate\s*\*\s*time\s*\*\s*principal
 I = P * R * T
+interest = principal * rate * time
 1000 * 0.05 * 3 = 150.0
 interest = 150.0
 print("The simple interest is: {interest}")
