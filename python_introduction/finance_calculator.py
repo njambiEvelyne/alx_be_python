a13c63ad3f1247f3ae6c86292d21ca8a89e96e0d
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-12 07:40:47 +0300

    Correction on formula

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
 input("Enter your monthly income: ")
-input("Enter your monthly expenses:")
+input("Enter your total monthly expenses:")
 print("Your monthly savings are $1000")
 print("Projected savings after one year , with interest, is: $12600")
 monthly income = 5000
 monthly expenses = 4000
-Monthly Savings = monthly income - monthly expenses
+monthly Savings = monthly income - monthly expenses
 (Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
 monthly_savings\s*=\s*(monthly_income\s*-\s*monthly_expenses|float\s*\(\s*monthly_income\s*\)\s*-\s*float\s*\(\s*monthly_expenses\s*\))
 
