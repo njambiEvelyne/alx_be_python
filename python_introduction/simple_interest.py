5ce096b7b4cb28ba813302065184c74d30dfa6f9
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-12 08:59:01 +0300

    Execution Correction

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,6 @@
 principal = 1000
 rate = 0.05
 time = 3
-interest\s*=\s*principal\s*\*\s*rate\s\*\s*time
-interest\s*=\s*rate\s*\*\s*principal\s*\*\s*time
-interest\s*=\s*time\s*\*\s*rate\s*\*\s*principal
-interest\s*=\s*principal\s*\*\s*time\s*\*\s*rate
-interest\s*=\s*time\s*\*\s*principal\s*\*\s*rate
-interest\s*=\s*rate\s*\*\s*time\s*\*\s*principal
 I = P * R * T
 interest = principal * rate * time
 1000 * 0.05 * 3 = 150.0
