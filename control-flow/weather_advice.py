f06c5d35a6e355126773f62a5ed85b576caef197
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-18 08:02:11 +0300

    Added prompt message

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,6 @@
+#weather = ["sunny", "rainy", "cold"]
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+
+#Avdvise for a sunny day
+if weather == "sunny":
+  print("Wear a t-shirt and sunglasses.")
