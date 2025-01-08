85b82ac539198381223609230fd66511cbc8b2ae
Ng'ang'a Evelyne Njambi <evelynenganga76@gmail.com>
2025-01-08 11:18:13 +0300

    added f-string format

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 def display_menu():  
     """Display the menu options for the shopping list manager."""  
-    print("\nShopping List Manager")  
+    print(f"\nShopping List Manager")  
     print("1. Add Item")  
     print("2. Remove Item")  
     print("3. View List")  
