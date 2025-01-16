ed9fa3188eef5057ad9ab3d9cecd7f23fe50f6fe
njambiEvelyne <evelynenganga76@gmail.com>
2025-01-16 14:49:06 +0300

    Test scripts

---
+++ ALX/module.py
@@ -0,0 +1,16 @@
+import unittest
+"""""
+Function to test
+"""
+def add(x,y):
+  return x+y
+
+def mult(a,b):
+  return a*b
+
+class TestOperation(unittest.TestCase):
+  def test_add(self):
+    self.assertEqual(add(6,7), 13)
+    self.assertEqual(mult(2,4), 8)
+
+unittest.main()
